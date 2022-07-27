6ec14d3af2aa22803ec49f96e0314cc410cf2bcf
Yannick wong <w-yannick@hotmail.fr>
2022-07-27 10:29:09 -0400

    remove login

--- cms/src/main/java/com/sg/cms/config/SecurityConfig.java
+++ cms/src/main/java/com/sg/cms/config/SecurityConfig.java
@@ -1,58 +1,58 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package com.sg.cms.config;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
-
-/**
- *
- * @author w-yan
- */
-@Configuration
-@EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
-//public class SecurityConfig {
-
-    @Autowired
-    public void configureGlobalInMemory(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("{noop}password").roles("USER")
-                .and()
-                .withUser("admin").password("{noop}password").roles("ADMIN", "USER");
-    }
-    
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http    
-                .authorizeRequests()
-                    //    "page To be protected"    "ROLE"   
-                    .antMatchers("/admin").hasRole("ADMIN")
-                    .antMatchers("/admin").hasRole("ADMIN")
-                    .antMatchers("/", "/home").permitAll()
-                    .antMatchers("/css/**", "/js/**", "/fonts/**").permitAll()
-                    .anyRequest().hasRole("USER")
-//                    .anyRequest().permitAll()
-                .and()
-                .formLogin()
-                    .loginPage("/login")
-                    .defaultSuccessUrl("/home",true)
-                    .failureUrl("/login?login_error=1")
-                    .permitAll()
-                .and()
-                .logout()
-                    .logoutSuccessUrl("/home")
-                    .logoutUrl("/logout")
-                    .permitAll();          
-    }
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package com.sg.cms.config;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+//
+//
+///**
+// *
+// * @author w-yan
+// */
+//@Configuration
+//@EnableWebSecurity
+//public class SecurityConfig extends WebSecurityConfigurerAdapter{
+////public class SecurityConfig {
+//
+//    @Autowired
+//    public void configureGlobalInMemory(AuthenticationManagerBuilder auth) throws Exception {
+//        auth
+//            .inMemoryAuthentication()
+//                .withUser("user").password("{noop}password").roles("USER")
+//                .and()
+//                .withUser("admin").password("{noop}password").roles("ADMIN", "USER");
+//    }
+//    
+//    @Override
+//    protected void configure(HttpSecurity http) throws Exception {
+//        http    
+//                .authorizeRequests()
+//                    //    "page To be protected"    "ROLE"   
+//                    .antMatchers("/admin").hasRole("ADMIN")
+//                    .antMatchers("/admin").hasRole("ADMIN")
+//                    .antMatchers("/", "/home").permitAll()
+//                    .antMatchers("/css/**", "/js/**", "/fonts/**").permitAll()
+//                    .anyRequest().hasRole("USER")
+////                    .anyRequest().permitAll()
+//                .and()
+//                .formLogin()
+//                    .loginPage("/login")
+//                    .defaultSuccessUrl("/home",true)
+//                    .failureUrl("/login?login_error=1")
+//                    .permitAll()
+//                .and()
+//                .logout()
+//                    .logoutSuccessUrl("/home")
+//                    .logoutUrl("/logout")
+//                    .permitAll();          
+//    }
+//}
