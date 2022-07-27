056441f11883cd6a0e55ec61049262c1fa856a11
Yannick wong <w-yannick@hotmail.fr>
2022-07-27 13:24:04 -0400

    Decouple body from blog

--- cms/Database/cmsDBTest.sql
+++ cms/Database/cmsDBTest.sql
@@ -7,14 +7,22 @@ create table Blog(
 	Id int primary key auto_increment,
     Title varchar(50),
     Description VARCHAR(50),
-    Content mediumtext,
     ExpiryDate Date
 );
+
+Create table BlogBody(
+	Id int primary Key,
+    Body mediumtext,
+    foreign key (Id)
+		references Blog(Id)
+);
+
 
 -- Hashtag
 create table Hashtag(
 	Id int primary key auto_increment,
     Name varchar(50)
+    
 );
 
 -- BlogHashtag bridge table
@@ -28,13 +36,13 @@ create table BlogHashtag(
 		references Hashtag(Id)
 );
 
-INSERT INTO Blog(Title, Description, Content, ExpiryDate) 
+INSERT INTO Blog(Title, Description, ExpiryDate) 
 VALUES 
-	("title 1 of the post","description 1","content 1","2022-07-26"),
-    ("title 2 of the post","description 2","content 2","2022-07-29"),
-    ("title 3 of the post","description 3","content 3","2022-08-01"),
-    ("title 4 of the post","description 4","content 4","2022-08-02"),
-    ("title 5 of the post","description 5","content 5","2022-08-02");
+	("title 1 of the post","description 1","2022-07-26"),
+    ("title 2 of the post","description 2","2022-07-29"),
+    ("title 3 of the post","description 3","2022-08-01"),
+    ("title 4 of the post","description 4","2022-08-02"),
+    ("title 5 of the post","description 5","2022-08-02");
     
 INSERT INTO Hashtag(Name) 
 VALUES 
@@ -56,6 +64,7 @@ VALUES
     
     
     
+
 
 
 
---
+++ cms/src/main/java/com/sg/cms/entity/BlogBody.java
@@ -0,0 +1,88 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package com.sg.cms.entity;
+
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Objects;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.ManyToMany;
+
+/**
+ *
+ * @author w-yan
+ */
+
+@Entity
+public class BlogBody {
+    
+    @Id 
+    int id;
+    
+    @Column
+    String body;
+    
+    
+    
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getBody() {
+        return body;
+    }
+
+    public void setBody(String body) {
+        this.body = body;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 5;
+        hash = 89 * hash + this.id;
+        hash = 89 * hash + Objects.hashCode(this.body);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final BlogBody other = (BlogBody) obj;
+        if (this.id != other.id) {
+            return false;
+        }
+        if (!Objects.equals(this.body, other.body)) {
+            return false;
+        }
+        return true;
+    }
+
+   
+
+   
+
+    
+    
+
+    
+    
+}
