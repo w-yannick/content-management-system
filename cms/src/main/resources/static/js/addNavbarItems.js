0f45f42258f74b6ceb6fb39bbdef84a9a63307a9
Yannick wong <w-yannick@hotmail.fr>
2022-07-29 14:47:55 -0400

    Fix logout not being displayed

--- cms/src/main/resources/static/js/addNavbarItems.js
+++ cms/src/main/resources/static/js/addNavbarItems.js
@@ -11,19 +11,16 @@ $(document).ready(function(){
                 + '<li class="nav-item" th:class="${activePage == "approvalPending"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/pendingApproval">pendingApproval</a></li>'
             );
             $('#navbarRight').append(
-                + '<li class="nav-item" th:class="${activePage == "login"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/logout">logout</a></li>'
+                '<li class="nav-item" th:class="${activePage == "logout"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/logout">logout</a></li>'
             );
             
             break;
         case "MANAGER":
             $('#navbarRight').prepend(
                 '<li class="nav-item" th:class="${activePage == "create"} ? "nav-item active" : "nav-item"><a class="nav-link" href="/create">Create</a></li>'            
-
- 
-                
-                    );
+            );
             $('#navbarRight').append(
-                '<li class="nav-item" th:class="${activePage == "login"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/logout">logout</a></li>'
+                '<li class="nav-item" th:class="${activePage == "logout"} ? "nav-item active" : "nav-item" ><a class="nav-link" href="/logout">logout</a></li>'
             );
             
         
