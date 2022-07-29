2ef1cdf3f5896fc561b8b7b425e7c479887ec574
Yannick wong <w-yannick@hotmail.fr>
2022-07-29 15:10:38 -0400

    Fix privilege

--- cms/src/main/java/com/sg/cms/config/SecurityConfig.java
+++ cms/src/main/java/com/sg/cms/config/SecurityConfig.java
@@ -35,6 +35,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter{
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http    
+                .csrf().disable()
                 .authorizeRequests()
                     //    "page To be protected"    "ROLE"   
                     .antMatchers("/create").hasRole("MANAGER")
